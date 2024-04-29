8f51d79c288d228606991916454e1953d9be7e5d
ayamedhat229 <ayamedhat773@gmail.com>
2024-04-29 18:09:33 +0300

    routing

--- src/app/modules/auth/auth-routing.module.ts
+++ src/app/modules/auth/auth-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './components/auth/auth/auth.component';
+import { RegisterComponent } from './components/register/register.component';
+import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
 
-const routes: Routes = [{ path: '', component: AuthComponent }];
+const routes: Routes = [{ path: '', component: AuthComponent },
+  {path:'register',component:RegisterComponent},
+  {path:'reset-password',component:ResetPasswordComponent},
+  {path:'forgot-password',component:ForgotPasswordComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -5,13 +5,15 @@ import { AuthRoutingModule } from './auth-routing.module';
 import { AuthComponent } from './components/auth/auth/auth.component';
 import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
 import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
+import { RegisterComponent } from './components/register/register.component';
 
 
 @NgModule({
   declarations: [
     AuthComponent,
     ForgotPasswordComponent,
-    ResetPasswordComponent
+    ResetPasswordComponent,
+    RegisterComponent
   ],
   imports: [
     CommonModule,
