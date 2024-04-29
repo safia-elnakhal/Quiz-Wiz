bc63bb14e1267ddaa587222325c3355fa16ccdb9
MaryemHaytham <maryemhaythamm@gmail.com>
2024-04-29 22:55:31 +0300

    Get profile and role

--- src/app/modules/auth/service/Auth.service.ts
+++ src/app/modules/auth/service/Auth.service.ts
@@ -3,13 +3,39 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ForgotPassword } from '../interfaces/forgot-password';
 import { ResetPassword } from '../interfaces/reset-password';
+import { jwtDecode } from 'jwt-decode';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  role : string | any = '';
 
-constructor(private _HttpClient:HttpClient) { }
+constructor(private _HttpClient:HttpClient) {
+  if(localStorage.getItem('userToken')!==null){
+    this.getProfile();
+    
+  }
+ }
+ getProfile(){
+  let encoded:any=localStorage.getItem('userToken');
+  console.log(encoded);
+  let decoded:any=jwtDecode(encoded);
+  localStorage.setItem('userRole',decoded.role);
+  localStorage.setItem('userName',decoded.first_name)
+  console.log(decoded)
+  this.getRole();
+}
+getRole(){
+  if(localStorage.getItem('userToken')!==null && localStorage.getItem('userRole')!==null){
+    this.role = localStorage.getItem('userRole') 
+  }
+  
+ 
+}
+
+
+
 
 OnForgotPassword(data:ForgotPassword):Observable<any>{
   return this._HttpClient.post('auth/forgot-password', data)
