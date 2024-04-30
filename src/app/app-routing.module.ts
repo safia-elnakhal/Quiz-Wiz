ce272bb93d6ffb837cb5a2b2e1f47c4710af0d33
MaryemHaytham <maryemhaythamm@gmail.com>
2024-04-30 17:09:09 +0300

    reset button

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'auth', loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule) },];
+const routes: Routes = [
+  { path: 'auth', loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule) },];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
