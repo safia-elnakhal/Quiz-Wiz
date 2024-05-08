b70bf06b2624b86f6ade41cfcd8b008b4ecc7229
ayamedhat229 <ayamedhat773@gmail.com>
2024-05-09 01:18:41 +0300

    edit

--- src/app/modules/instructors/modules/questions/questions.module.ts
+++ src/app/modules/instructors/modules/questions/questions.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { QuestionsRoutingModule } from './questions-routing.module';
 import { QuestionListComponent } from './question-list/question-list.component';
 import { SharedModule } from 'src/app/shared/shared.module';
