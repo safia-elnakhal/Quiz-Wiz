c8eda2f4f22258ff092a795e6b3d74ac8702c90e
MaryemHaytham <maryemhaythamm@gmail.com>
2024-05-13 01:17:33 +0300

    done

--- src/app/modules/dashboard/components/home/home.component.ts
+++ src/app/modules/dashboard/components/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Ihome } from 'src/app/modules/auth/interfaces/ihome';
 import { HomeService } from 'src/app/modules/auth/services/home.service';
+import { UserQuizService } from 'src/app/modules/students/services/user-quiz.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -11,11 +12,12 @@ export class HomeComponent implements OnInit{
   userRole = localStorage.getItem('userRole')
   //tableOfStudent:Ihome | any;
   tableOfStudent:Ihome[]=[];
-  //tableOfQuiz:any;
-  constructor(private _HomeService:HomeService){}
+  tableOfQuiz:any;
+  constructor(private _HomeService:HomeService, private _UserQuizService:UserQuizService){}
   ngOnInit(): void {
     this.getTopStudent()
     this.getTopQuizes()
+    this.getIncommingQuizzes()
   }
 
 getTopStudent(){
@@ -35,6 +37,19 @@ getTopQuizes(){
   this._HomeService.onGetTopQuestions().subscribe({
     next:(res)=>{
       console.log(res)
+    },
+    error:(err)=>{
+      console.log(err)
+    }
+  })
+}
+
+getIncommingQuizzes(){
+  this._UserQuizService.getIncommingQuizzes().subscribe({
+    next:(res)=>{
+    
+      this.tableOfQuiz=res;
+      console.log(this.tableOfQuiz)
     },
     error:(err)=>{
       console.log(err)
