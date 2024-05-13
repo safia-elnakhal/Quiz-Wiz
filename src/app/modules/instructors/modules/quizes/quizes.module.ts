98158d2f76b85dd6bc4cf815c8f4eeb37915b687
safia-elnakhal <safiaelnakhal@gmail.com>
2024-05-13 03:41:16 +0300

    merge quiz list completed and upcoming

--- src/app/modules/instructors/modules/quizes/quizes.module.ts
+++ src/app/modules/instructors/modules/quizes/quizes.module.ts
@@ -5,12 +5,14 @@ import { QuizesRoutingModule } from './quizes-routing.module';
 import { QuizListComponent } from './quiz-list/quiz-list.component';
 import { AddEditQuizComponent } from './add-edit-quiz/add-edit-quiz.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { ViewQuizComponent } from './view-quiz/view-quiz.component';
 
 
 @NgModule({
   declarations: [
     QuizListComponent,
-    AddEditQuizComponent
+    AddEditQuizComponent,
+    ViewQuizComponent
   ],
   imports: [
     CommonModule,
